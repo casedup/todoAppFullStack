13e336d51cf5da64f457ee6366d4f614cf00d404
casedup <jatavais@trillnetworks.com>
2021-12-08 00:22:17 -0400

    submit and delete functionality added

--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
 
-const apiUrl = '/api/todos';
+const apiUrl = '/api/todos/';
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             todos: []
         }
+        this.addTodo = this.addTodo.bind(this);
     }
     componentDidMount() {
         this.loadTodos();
@@ -28,18 +30,72 @@ class TodoList extends Component {
                     }
                 }
                 return resp.json();
-            }).then(todos => this.setState({ todos }));
+            })
+            .then(todos => this.setState({ todos }));
+    }
+    addTodo(val) {
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type':'application/json',
+            }),
+            body: JSON.stringify({name: val})
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = { errorMessage: data.message };
+                        throw err;
+                    })
+                } else {
+                    let err = { errorMessage: 'Please try again later, server not responding.' }
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(newTodo => {
+            this.setState({todos: [...this.state.todos, newTodo]})
+        });
+    }
+    deleteTodo(id) {
+        const deleteUrl = apiUrl+id;
+        fetch(deleteUrl, {
+            method: 'DELETE'
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = { errorMessage: data.message };
+                        throw err;
+                    })
+                } else {
+                    let err = { errorMessage: 'Please try again later, server not responding.' }
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(() => {
+            const todos = this.state.todos.filter(todo => todo._id !== id);
+            this.setState({todos:todos});
+        });
+
     }
     render() {
         const todos = this.state.todos.map((t) => (
             <TodoItem
             key={t._id}
             {...t}
+            onDelete={this.deleteTodo.bind(this,t._id)}
             />
         ));
         return (
             <>
                 <h1>Todo List</h1>
+                <TodoForm addTodo={this.addTodo} />
                 <ul>{todos}</ul>     
             </>
             
